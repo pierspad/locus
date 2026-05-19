e8320512c13dc2435439d04f35013acff0062bac
pierspad <pierpaolospadafora23+github@gmail.com>
2026-05-19 21:01:35 +0200

    struttura basilare

--- include/locus/geometry.hpp
+++ include/locus/geometry.hpp
@@ -1,8 +1,45 @@
+#include <algorithm>
 
 
+struct Point{
+    int x;
+    int y;
+};
 
-struct Rectangle{
-    Vec2 topleft;
+struct Size{
     int width;
     int height;
+};
+
+struct Rectangle{
+    Point top_left;
+    Size size;
+};
+
+
+
+int left(Rectangle rect){
+    return rect.top_left.x;
+}
+
+int right(Rectangle rect){
+    
+    return rect.top_left.x + rect.size.width;
+}
+
+int top(Rectangle rect){
+    return rect.top_left.y;
+}
+
+int bottom(Rectangle rect){    
+    return rect.top_left.y + rect.size.height;
+}
+
+
+int overlap_width(Rectangle a, Rectangle b){
+    return std::max(0, std::min(right(a), right(b)) - std::max(left(a), left(b)));
+}
+
+int overlap_height(Rectangle a, Rectangle b){
+    return std::max(0, std::min(bottom(a), bottom(b)) - std::max(top(a), top(b)));
 }
